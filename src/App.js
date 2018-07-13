083a17b38551d7f6f06da70c09d38683019de8e6
Dave Moen <dave.moen@protolabs.com>
2018-07-13 14:08:57 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './SailsAlessmall.jpg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <img src={logo} alt="logo" />
+      </div>
+    );
+  }
+}
+
+export default App;
